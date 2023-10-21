e33901ba28dcb963904e2fa631f94e0eafb2583c
Yonatan Zewude <yonatanzewude@hotmail.com>
2023-10-21 11:28:47 +0200

    add button function is added in main.js

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
 import "./style.css";
+
+const addButton = document.getElementById("add-button");
+const ulList = document.querySelector("#ulList");
+const liLists = document.querySelector("li");
+const userInput = document.querySelector("#task-input");
+
+function createNewList() {
+  const newList = document.createElement("li");
+  newList.innerHTML = userInput.value;
+
+  ulList.appendChild(newList);
+}
+
+addButton.addEventListener("click", createNewList);
+
+function taskIsDone() {
+  for (let i = 0; i < liLists.length; i++) {
+    const done = document.createElement("button");
+    done.innerHTML = "Done";
+    liLists.appendChild(done);
+  }
+}
+liLists.addEventListener("click", taskIsDone);
